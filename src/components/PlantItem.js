8fa4115dd4166a6ff70e2e1b5628522ed65e8a76
Tiavina19 <hei.tiavina@gmail.com>
2025-06-23 09:32:59 +0300

    feat: add cart state management with open/close and reset functionality

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -2,8 +2,12 @@ import CareScale from "./CareScale";
 import "../styles/PlantItem.css";
 
 function PlantItem({ id, cover, name, water, light }) {
+  function handleClick(e) {
+    alert(`Vous voulez acheter 1 ${name} ? Très bon choix 🌱✨`);
+  }
+
   return (
-    <li key={id} className="lmj-plant-item">
+    <li className="lmj-plant-item" onClick={handleClick}>
       <img className="lmj-plant-item-cover" src={cover} alt={`${name} cover`} />
       {name}
       <div>
